f781887286fcf955ff992b6e9a510c9b7c0cec93
kobra-soft <code.softwaredev@gmail.com>
2024-07-27 19:58:01 +0100

    Footer Added / Bottom Content Added / Header fixes

--- app/page.tsx
+++ app/page.tsx
@@ -49,46 +49,50 @@ export default function Home() {
 
   return (
     <main className="flex flex-col">
-      <header className="w-full fixed py-5 px-8 header-footer-background-colour">
+      <header className="header bg-white z-50 w-full fixed top-0  py-5 px-8 header-footer-background-colour">
         <div className="flex justify-between items-center">
           <h1 className="text-3xl cursor-pointer">Kobra-Soft</h1>
           <button onClick={toggleMenu} className="cursor-pointer">
-            {isMenuOpen ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="28"
-                height="28"
-                fill="white"
-                viewBox="0 0 16 16"
-                className="transition-transform duration-300"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M14.707 1.293a1 1 0 010 1.414L9.414 8l5.293 5.293a1 1 0 01-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 01-1.414-1.414L6.586 8 1.293 2.707a1 1 0 011.414-1.414L8 6.586l5.293-5.293a1 1 0 011.414 0z"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="28"
-                height="28"
-                fill="white"
-                viewBox="0 0 16 16"
-                className="transition-transform duration-300"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-                />
-              </svg>
-            )}
+            <div className="w-7 h-7 flex items-center justify-center">
+              {isMenuOpen ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="white"
+                  viewBox="0 0 16 16"
+                  className="transition-transform duration-300"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M14.707 1.293a1 1 0 010 1.414L9.414 8l5.293 5.293a1 1 0 01-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 01-1.414-1.414L6.586 8 1.293 2.707a1 1 0 011.414-1.414L8 6.586l5.293-5.293a1 1 0 011.414 0z"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="28"
+                  height="28"
+                  fill="white"
+                  viewBox="0 0 16 16"
+                  className="transition-transform duration-300"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+                  />
+                </svg>
+              )}
+            </div>
           </button>
         </div>
       </header>
 
       {/* // Container for the main content */}
-      <div id="section1"
-      className="main_container_div px-8 pt-36 bg-[#3d11ff] h-screen w-full">
+      <div
+        id="section1"
+        className="main_container_div px-8 pt-36 bg-[#3d11ff] h-screen w-full"
+      >
         <h2 className="">
           Full stack web & mobile application development, specialising in the
           building of bespoke applications.
@@ -120,8 +124,10 @@ export default function Home() {
       </div>
 
       {/* Second Container for additional content */}
-      <div id="section2"
-      className="second_container_div px-8 pt-36 bg-[#2a0fa2] h-screen w-full">
+      <div
+        id="section2"
+        className="second_container_div px-8 pt-36 bg-[#2a0fa2] h-screen w-full"
+      >
         <h2>Additional content 2 or sections can go here.</h2>
         <h2>
           Describe more services, showcase projects, or include testimonials.
@@ -135,9 +141,11 @@ export default function Home() {
         </h2>
       </div>
 
-        {/* Third Container for additional content */}
-        <div  id="section3"
-        className="third_container_div px-8 pt-36 bg-[#1d0a70] h-screen w-full">
+      {/* Third Container for additional content */}
+      <div
+        id="section3"
+        className="third_container_div px-8 pt-36 bg-[#1d0a70] h-screen w-full"
+      >
         <h2>Additional content 3 or sections can go here.</h2>
         <h2>
           Describe more services, showcase projects, or include testimonials.
@@ -151,55 +159,151 @@ export default function Home() {
         </h2>
       </div>
 
-      {/*       <div className="flex justify-center border-t border-[#272727] w-full h-full">
-        <Image
-          src="/logo-javascript.svg"
-          alt="JavaScript Logo"
-          width={180}
-          height={250}
-          className="next-logo-black my-32 mx-auto"
-        />
-        <Image
-          src="/logo-npm.svg"
-          alt="NPM Logo"
-          width={180}
-          height={250}
-          className="next-logo-black my-32 mx-auto"
-        />
-        <Image
-          src="/logo-html5.svg"
-          alt="HTML5 Logo"
-          width={180}
-          height={250}
-          className="next-logo-black my-32 mx-auto"
-        />
+      <div className="flex flex-col items-center w-full">
+        <div className="flex justify-center border-t border-[#272727] w-full">
+          <Image
+            src="/logo-javascript.svg"
+            alt="JavaScript Logo"
+            width={180}
+            height={250}
+            className="next-logo-black my-32 mx-auto"
+          />
+          <Image
+            src="/logo-npm.svg"
+            alt="NPM Logo"
+            width={180}
+            height={250}
+            className="next-logo-black my-32 mx-auto"
+          />
+          <Image
+            src="/logo-html5.svg"
+            alt="HTML5 Logo"
+            width={180}
+            height={250}
+            className="next-logo-black my-32 mx-auto"
+          />
+        </div>
+        <div className="flex justify-center bg-black/15 w-full">
+          <Image
+            src="/logo-android.svg"
+            alt="Android Logo"
+            width={180}
+            height={250}
+            className="next-logo-black my-32 mx-auto"
+          />
+          <Image
+            src="/logo-react.svg"
+            alt="React Logo"
+            width={180}
+            height={250}
+            className="next-logo-black my-32 mx-auto"
+          />
+          <Image
+            src="/logo-apple.svg"
+            alt="Apple Logo"
+            width={180}
+            height={250}
+            className="next-logo-black my-32 mx-auto"
+          />
+        </div>
       </div>
-      <div className="flex justify-center bg-black/15 w-full">
-        <Image
-          src="/logo-android.svg"
-          alt="Android Logo"
-          width={180}
-          height={250}
-          className="next-logo-black my-32 mx-auto"
-        />
-        <Image
-          src="/logo-react.svg"
-          alt="React Logo"
-          width={180}
-          height={250}
-          className="next-logo-black my-32 mx-auto"
-        />
-        <Image
-          src="/logo-apple.svg"
-          alt="Apple Logo"
-          width={180}
-          height={250}
-          className="next-logo-black my-32 mx-auto"
-        />
-      </div>
-      <div className="flex justify-center bg-black/15 border-black w-full">
-      <h4>|</h4>
+
+      {/* // TESTING MIDDLE SCREEN POSITIONER | REMOVE LATER */}
+      {/* <div className="flex justify-center bg-black/15 border-black w-full">
+        <h4>|</h4>
       </div> */}
+
+      <footer className="footer mt-auto text-white/85 px-20 py-24 w-full header-footer-background-colour">
+        <div className="flex justify-between items-center h-full">
+          {/* LEFTSIDE */}
+          <div className="flex flex-col justify-center items-center">
+            <div className="cursor-default">
+              © 2024{" "}
+              <a
+                href="https://github.com/Kobra-soft"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className="mx-1 cursor-pointer hover:text-white ">
+                  Kobra-Soft <span className="text-2xl">™</span>
+                </span>
+              </a>
+            </div>
+            <h4 className="footer_2nd_font text-[#636364] text-center cursor-default">
+              All rights reserved
+            </h4>
+          </div>
+          {/* SVG - Icons Row */}
+          <div className="absolute left-1/2 transform -translate-x-1/2">
+            <div className="flex gap-7">
+              <Image
+                src="/logo-npm.svg"
+                alt="Npm Logo"
+                title="Node Package Manager"
+                width={50}
+                height={50}
+                className="next-logo-white hover:scale-110"
+              />
+              <Image
+                src="/typescript-svgrepo-com.svg"
+                alt="Typescript Logo"
+                title="TypeScript"
+                width={60}
+                height={60}
+                className="next-logo-white hover:scale-110"
+              />
+              <Image
+                src="/logo-nodejs2.svg"
+                alt="NodeJS Logo"
+                title="NodeJS"
+                width={50}
+                height={50}
+                className="next-logo-white hover:scale-110"
+              />
+
+              <Image
+                src="/logo-react.svg"
+                alt="React Logo"
+                title="React"
+                width={50}
+                height={50}
+                className="next-logo-white hover:scale-110"
+              />
+              <Image
+                src="/next.svg"
+                alt="Next.js Logo"
+                title="Next.js"
+                width={60}
+                height={60}
+                className="next-logo-white hover:scale-110"
+              />
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                title="Vercel"
+                width={60}
+                height={60}
+                className="next-logo-white hover:scale-110"
+              />
+            </div>
+          </div>
+          {/* RIGHTSIDE */}
+          <div className="flex flex-row gap-6 items-center text-white/85">
+            <a href="#" className=" hover:text-white">
+              FAQ
+            </a>
+            <a href="#" className=" hover:text-white">
+              Cookies
+            </a>
+            <a href="#" className=" hover:text-white">
+              Privacy
+            </a>
+            <a href="#" className=" hover:text-white">
+              T & C
+            </a>
+          </div>
+        </div>
+      </footer>
     </main>
   );
 }
