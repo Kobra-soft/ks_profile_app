622c6442f6ab6c929e1f42508ca4e6e67e0e86f3
kobra-soft <code.softwaredev@gmail.com>
2024-08-12 13:26:50 +0100

    Testing - Responsive Mobile Version

--- app/page.tsx
+++ app/page.tsx
@@ -140,22 +140,44 @@ export default function Home() {
       {/* Third Container for additional content */}
       <div
         id="section3"
-        className="third_container_div px-12 pt-36 bg-[#000000] h-screen w-full text-4xl"
+        className="third_container_div px-12 pt-36 bg-[#ffffff] h-screen w-full text-4xl"
       >
-        <h3>Additional content 3 or sections can go here.</h3>
-        <h3>
+        <h4>Additional content 3 or sections can go here.</h4>
+        <h4>
           Describe more services, showcase projects, or include testimonials.
-        </h3>
-        <h3>Each section can have its unique content and styling as needed.</h3>
-        <h3>
+        </h4>
+        <h4>Each section can have its unique content and styling as needed.</h4>
+        <h4>
           This setup allows for creating a full-page scrolling experience.
-        </h3>
-        <h3>
+        </h4>
+        <h4>
           Consider navigation options for users to easily jump between sections.
+        </h4>
+      </div>
+
+            {/* // Container for the main content */}
+            <div
+        id="section1"
+        className="main_container_div pt-80 
+      bg-black h-screen w-full px-8 lg:pl-[12.5rem]"
+      >
+        <h2 className="text-[50px] md:text-[50px] lg:text-[120px] mb-2.5 cursor-pointer">
+          SOLUTIONS
+        </h2>
+
+        <h3
+          className="text-[20px] md:text-[20px] lg:text-[33px] font-extralight text-[#cbced3]
+        md:w-[60%]  xl:w-[45%]"
+        >
+          We empower brands with{" "}
+          <span className="">innovative digital solutions</span>, crafting
+          <span className=""> user-centric experiences </span>
+          that enhance <span className="">brand prescence </span>and drive
+          <span className=""> business growth.</span>
         </h3>
       </div>
 
-      <div className="flex flex-col items-center w-full bg-[#ff0059]">
+      {/* <div className="flex flex-col items-center w-full bg-[#ff0059]">
         <div className="flex justify-center border-t  border-[#272727] w-full ">
           <Image
             src="/logo-javascript.svg"
@@ -202,21 +224,21 @@ export default function Home() {
             className="next-logo-black my-32 mx-auto"
           />
         </div>
-      </div>
+      </div> */}
 
       {/* // TESTING MIDDLE SCREEN POSITIONER | REMOVE LATER */}
       {/* <div className="flex justify-center bg-black/15 border-black w-full">
         <h4>|</h4>
       </div> */}
 
-
-      
-      {/* // FOOTER FOR DESKTOP SITE! Hide for mobile */}
-      <footer className="footer mt-auto text-white/75 
+      {/* // FOOTER FOR DESKTOP SITE! Hide for mobile < */}
+      <footer
+        className="footer mt-auto text-white/75 
       xl:px-8 
       py-10 w-full 
       header-footer-background-colour 
-      hidden md:hidden xl:block">
+      hidden md:hidden xl:block"
+      >
         <div className="flex justify-between items-center h-full">
           {/* LEFTSIDE */}
           <div className="flex flex-col justify-center items-center">
@@ -249,23 +271,85 @@ export default function Home() {
           </div>
           {/* RIGHTSIDE */}
           <div className="flex flex-row gap-7 items-center text-white/75 text-lg">
-            <a href="#" className=" hover:text-[#ff0479] hover-underline hover:scale-105">
+            <a
+              href="#"
+              className=" hover:text-[#ff0479] hover-underline hover:scale-105"
+            >
               About
             </a>
-            <a href="#" className=" hover:text-[#ff0479] hover-underline hover:scale-105">
+            <a
+              href="#"
+              className=" hover:text-[#ff0479] hover-underline hover:scale-105"
+            >
               Cookies
             </a>
-            <a href="#" className=" hover:text-[#ff0479] hover-underline hover:scale-105">
+            <a
+              href="#"
+              className=" hover:text-[#ff0479] hover-underline hover:scale-105"
+            >
               Privacy
             </a>
-            <a href="#" className=" hover:text-[#ff0479] hover-underline hover:scale-105">
+            <a
+              href="#"
+              className=" hover:text-[#ff0479] hover-underline hover:scale-105"
+            >
               Contact Us
             </a>
           </div>
         </div>
       </footer>
 
-
+      {/* // FOOTER FOR MOBILE/TABLETS! Hide for desktop > */}
+      <footer
+        className="footer text-white/75 
+        px-4 md:px-8 
+        py-0 w-full 
+        header-footer-background-colour 
+        block md:block xl:hidden"
+      >
+        <div className="flex flex-col justify-center items-center h-full pt-8">
+          <div className="flex gap-8 items-center text-white/75">
+            <a href="#" className="hover:text-[#ff0479] hover-underline">
+              About
+            </a>
+            <a href="#" className="hover:text-[#ff0479] hover-underline">
+              Cookies
+            </a>
+            <a href="#" className="hover:text-[#ff0479] hover-underline">
+              Privacy
+            </a>
+            <a href="#" className="hover:text-[#ff0479] hover-underline">
+              Contact Us
+            </a>
+          </div>
+          <div className="flex gap-4 items-center pt-11">
+            <NpmLogo />
+            <TypescriptLogo />
+            <NodejsLogo />
+            <ReactLogo />
+          </div>
+          <div className="flex gap-4 items-center mb-8">
+            <NextLogo />
+            <VercelLogo />
+          </div>
+          <div className="flex items-center justify-center cursor-default text-md text-center mb-4">
+            <span className="text-[#ffffff]/75">© 2024{" "}</span>
+            
+            <a
+              href="https://github.com/Kobra-soft"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="mx-0.5 pl-1 cursor-pointer hover:text-[#ff0479] text-[#ffffff]/75">
+                Kobra-Soft ™
+              </span>
+            </a>
+            <span className=" text-[#7b7b7d] cursor-default text-md mx-1">
+              All rights reserved
+            </span>
+          </div>
+        </div>
+      </footer>
     </main>
   );
 }
